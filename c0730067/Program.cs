3f147393a880f129b5f4f31bd17ed3c3d5c0c91e
visualstudio <visualstudio@122-PC34>
2019-04-10 09:33:01 -0400

    second commit

--- c0730067/Program.cs
+++ c0730067/Program.cs
@@ -8,8 +8,29 @@ namespace c0730067
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run() { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+            //Read file using StreamReader.Reads fil line by line
+            using (StreamReader file = new StreamReader(@"U:\Users\730067\beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln=file.ReadLine())!=null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter}lines.");
+            }
+        }
+
     }
 }
