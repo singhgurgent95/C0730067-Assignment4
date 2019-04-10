efed4d36e3d5a1da841e054c338a274b14220b8a
visualstudio <visualstudio@122-PC34>
2019-04-10 11:49:09 -0400

    section D

--- c0730067/Program.cs
+++ c0730067/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,7 +30,11 @@ namespace c0730067
                 string line;
                 int counter = 0;
                 int a = 0, myWord = 1;
-                int averageLetterPerWord;
+                float averageLetterPerWord;
+                ArrayList lineNumbers = new ArrayList();
+                ArrayList lineNumbers2 = new ArrayList();
+                int linenum = 1;
+                lineNumbers.Add(22);
 
                 while ((line = sr.ReadLine()) != null)
                 {
@@ -40,6 +43,23 @@ namespace c0730067
                     FindNumberOfBlankSpaces(line);
                     counter++;
 
+                    // SECTION C
+                    if (line.Substring(0).Contains("Sea") || line.Substring(0).Contains("sea") && line.Substring(0).Contains("Fare") || line.Substring(0).Contains("fare"))
+                    {
+                        lineNumbers.Add(linenum);
+                    }
+
+                    // SECTION D
+                    if (line.Substring(0).Contains("fare") || line.Substring(0).Contains("Fare"))
+                    {
+                        if (!(line.Substring(0).Contains("war") || line.Substring(0).Contains("War")))
+                        {
+                            lineNumbers2.Add(linenum);
+                        }
+
+                    }
+
+                    // COUNTING THE NUMBER OF WORDS  SECTION B
                     while (a <= line.Length - 1)
                     {
                         if (line[a] == ' ' || line[a] == '\n' || line[a] == '\t')
@@ -50,13 +70,31 @@ namespace c0730067
                     }
                     a = 0;
 
+                    linenum++;  // SECTION C
                 }
-
+                // COUNTING THE NUMBER OF AVERAGE LETTERS IN WORD SECTION E
                 averageLetterPerWord = counterletters / countSpaces;
 
+                // SECTION A: NUMBER OF LINES
                 Console.WriteLine("\n\n\n\n********************************\nThe number of lines in the paragraph is " + counter);
                 Console.WriteLine("The number of words in paragraph is " + myWord);
                 Console.WriteLine("The number of average letters per word is  " + averageLetterPerWord);
+
+
+                // SECTION C: WORDS WHICH CONTAINS BOTH SEA AND FARE
+                Console.WriteLine("The line which contains both sea and fare\n");
+                foreach (int i in lineNumbers)
+                {
+                    Console.Write(i + " ");
+                }
+
+                // SECTION C: WORDS WHICH CONTAINS BOTH SEA AND FARE
+                Console.WriteLine("The line which contains Fare, but not War\n");
+                foreach (int i in lineNumbers2)
+                {
+                    Console.Write(i + " ");
+                }
+
                 Console.ReadLine();
             }
 
@@ -72,4 +110,3 @@ namespace c0730067
         }
     }
 }
-
